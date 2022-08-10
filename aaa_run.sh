d09f578c3230a2f9000168f23bcf4f25b1a2dd3d
Kamil.Jarosz <kamil.rafal.jarosz@gmail.com>
2022-08-10 10:10:07 +0200

    BufferSize added to script

--- aaa_run.sh
+++ aaa_run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-insmod /lib/modules/5.15.54-yocto-standard/extra/aaa_kernel.ko
+insmod /lib/modules/5.15.54-yocto-standard/extra/aaa_kernel.ko bufferSize=32
 mkfifo /var/log/test_file
 aaa /var/log/test_file &
